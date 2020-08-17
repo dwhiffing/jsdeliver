766ff0bf01ff9c59f95a8380d89f727ee5cd63ce
Daniel Whiffing <danielwhiffing@gmail.com>
2020-08-17 13:26:54 -0400

    test

---
+++ apparel.js
@@ -0,0 +1,13 @@
+;(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) : factory()
+})(function () {
+  'use strict'
+
+  window.cardify.loadCardifyIndustry(
+    'Apparel & Accessories',
+    {
+      'Apparel & Accessories': window.cardify.CHART_COLORS[0],
+    },
+    [],
+  )
+})
